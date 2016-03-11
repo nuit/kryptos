f86d80ef0ff75c6e4ba93aac86dc09e418ed2937
nuit <nuit@protonmail.com>
2016-03-11 00:18:46 -0300

    Update analysis.py

--- ciphers/analysis.py
+++ ciphers/analysis.py
@@ -1,19 +1,46 @@
 # Frequency Analysis
 # by: nuit
 
-text=raw_input('[!] text: ')
-letters=' '.join(text).split()        
+import sys
+
+G='\033[92m'
+B='\033[94m'
+ENDC='\033[0m'
             
-d=dict()
-def count_letters(letters):
+d,b={},{}
+def countchars(filename):
+	f=open(filename, "r").read()
+	f=''.join(f.split())
+	chars=' '.join(f).split()
+	t=len(f)
+
+	for c in chars:
+		d[c]=chars.count(c) 
+	
+	print G+'\n[+] Total chars:',str(t)+ENDC
+	
+	print B+'\n','*'*6,'Characters:','*'*6+ENDC
 	j=0
-	for l in letters:
-		d[l]=letters.count(l) 
-	t=len(letters)
-	print '[+] total letters:',str(t)
 	for i in sorted(d, key=d.get, reverse=True): 
 		p=round(float(d[i])/float(t)*100,2)
 		print '['+str(j)+'] '+(i+': '+str(d[i]))+' - '+str(p)+'%'
 		j+=1
 
-count_letters(letters)		
+	for x in range(t):
+		n=f[x:x+2]
+		b[n]=f.count(n)
+
+	z=0
+	print B+'\n\n','*'*6,'Bigrams:','*'*6+ENDC
+	for key, value in sorted(b.iteritems(), key=lambda (k,v): (v,k), reverse=True):
+		p=round(float(b[key])/float(t)*100,2)
+		print '['+str(z)+'] '+(key+': '+str(b[key]))+' - '+str(p)+'%'
+		z+=1
+
+if len(sys.argv)>=3:
+	if sys.argv[1]=='-f':
+		filename=sys.argv[2]
+		countchars(filename)
+else:
+	print '\nUsage: python analysis.py -f <filename>\n'
+	
